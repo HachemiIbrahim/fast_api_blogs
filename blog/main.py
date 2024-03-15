201296d4484d847ec947b1f9ac3b3503d344adc6
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-03-15 17:33:33 +0100

    storing a blog

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
+from sqlalchemy.orm import Session
 from . import schema , models
-from .database import Base , engine
+from .database import Base , engine , SessionLocal
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post("/blog")
-def creating(request:schema.Blog):
-    return request
+def creating(request:schema.Blog , db : Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title ,body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
