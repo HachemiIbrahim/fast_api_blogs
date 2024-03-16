b235061ce859c2a98999ffbdec8c6767907faf66
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-03-16 12:24:36 +0100

    exception and statuse code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Depends
+from fastapi import FastAPI , Depends , status , HTTPException , Response
 from sqlalchemy.orm import Session
 from . import schema , models
 from .database import Base , engine , SessionLocal
@@ -14,7 +14,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/blog")
+@app.post("/blog" , status_code=status.HTTP_201_CREATED)
 def creating(request:schema.Blog , db : Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title ,body = request.body)
     db.add(new_blog)
@@ -27,7 +27,14 @@ def show_all( db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blogs/{id}")
-def get(id ,  db : Session = Depends(get_db)):
+@app.delete("/blog/{id}" , status_code=status.HTTP_200_OK)
+def delete(id ,  response : Response,db : Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete()
+    db.commit()
+
+@app.get("/blogs/{id}" , status_code=status.HTTP_200_OK)
+def get(id ,  response : Response,db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"the blog with the id {id} is not found")
     return blog
