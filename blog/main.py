90c71a07176396aca78ad1ed7305b7ced8c24bba
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-03-16 18:41:16 +0100

    update methose

--- blog/main.py
+++ blog/main.py
@@ -27,10 +27,23 @@ def show_all( db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.delete("/blog/{id}" , status_code=status.HTTP_200_OK)
+@app.delete("/blog/{id}" , status_code=status.HTTP_204_NO_CONTENT)
 def delete(id ,  response : Response,db : Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete()
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"the blog with the id {id} is not found")
+    blog.delete(synchronize_session=False)
     db.commit()
+    
+    
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id,request:schema.Blog,db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'detail with id {id} not found')
+    blog.update(request.dict())
+    db.commit()
+    return {'updated'}
 
 @app.get("/blogs/{id}" , status_code=status.HTTP_200_OK)
 def get(id ,  response : Response,db : Session = Depends(get_db)):
