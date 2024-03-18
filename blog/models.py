7a5b8295a6bd89d22fdc08d82fce3e5a17ff6e3c
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-03-18 12:49:27 +0100

    relationships

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String , ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
@@ -9,7 +9,9 @@ class Blog(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer , ForeignKey("users.id"))
     
+    writenBy = relationship("User", back_populates="blogs")
 
 class User(Base):
     __tablename__ = "users"
@@ -18,5 +20,6 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
-    
+
+    blogs = relationship("Blog", back_populates="writenBy")
 
--- blog/schema.py
+++ blog/schema.py
@@ -1,23 +1,35 @@
+from typing import List
 from pydantic import BaseModel
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title:str
     body:str
     
-class ShowBlog(BaseModel):
-    title:str
-    body:str
+class Blog(BlogBase):
     class Config:
         orm_mode = True
     
-class User(BaseModel):
+    
+class UserBase(BaseModel):
     name:str
     email:str
     password:str
     
+class User(UserBase):    
+    class Config:
+        orm_mode = True
+
 class SHowUser(BaseModel):
     name:str
     email:str
+    blogs : List[Blog] =[]
     class Config:
         orm_mode = True
     
+    
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+    writenBy:User
+    class Config:
+        orm_mode = True
